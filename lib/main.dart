584e6b37075d59a60c2061456034ea6ae179591f
Luiz Guilherme Muraro <luizmuraro@gmail.com>
2019-10-01 13:27:41 -0300

    Adiciona interface

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,15 @@ import 'dart:convert';
 const request = "https://api.hgbrasil.com/finance?format=json&key=31b1a2e5";
 
 void main() async {
-  runApp(MaterialApp(home: Home()));
+  runApp(MaterialApp(
+      home: Home(),
+      theme: ThemeData(
+        hintColor: Colors.amber,
+          primaryColor: Colors.white,
+          inputDecorationTheme: InputDecorationTheme(
+              enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.white))
+          )
+      )));
 }
 
 Future<Map> getData() async {
@@ -21,6 +29,10 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  double dolar;
+  double euro;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -51,7 +63,72 @@ class _HomeState extends State<Home> {
                         textAlign: TextAlign.center,)
                   );
                 } else {
-                  return Container(color: Colors.yellow);
+                    dolar = snapshot.data["results"]["currencies"]["USD"]["buy"];
+                    euro = snapshot.data["results"]["currencies"]["EUR"]["buy"];
+
+                  return SingleChildScrollView(
+                    padding: EdgeInsets.all(10.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: <Widget>[
+                        Icon(Icons.monetization_on, size: 150.0, color: Colors.amber),
+
+                        TextField(
+                          decoration: InputDecoration(
+                            labelText: "Reais",
+                            labelStyle: TextStyle(color: Colors.amber),
+                            border: OutlineInputBorder(),
+                            prefixText: "R\$",
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.white),
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color:Colors.amber,)
+                            )
+                          ),
+                          style: TextStyle(
+                            color: Colors.amber, fontSize: 25.0
+                          ),
+                        ),
+                        Divider(),
+                        TextField(
+                          decoration: InputDecoration(
+                              labelText: "Dólares",
+                              labelStyle: TextStyle(color: Colors.amber),
+                              border: OutlineInputBorder(),
+                              prefixText: "US\$",
+                              focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white),
+                        ),
+                      enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color:Colors.amber,)
+                      )
+                          ),
+                          style: TextStyle(
+                              color: Colors.amber, fontSize: 25.0
+                          ),
+                        ),
+                        Divider(),
+                        TextField(
+                          decoration: InputDecoration(
+                              labelText: "Euros",
+                              labelStyle: TextStyle(color: Colors.amber),
+                              border: OutlineInputBorder(),
+                              prefixText: "€",
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.white),
+                              ),
+                              enabledBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(color:Colors.amber,)
+                              )
+                          ),
+                          style: TextStyle(
+                              color: Colors.amber, fontSize: 25.0
+                          ),
+                        )
+                      ],
+                    ),
+                  );
                 }
             }
           }),
